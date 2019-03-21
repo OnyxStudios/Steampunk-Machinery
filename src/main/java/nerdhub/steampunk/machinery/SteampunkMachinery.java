3fb6afdda29e393d918535f96dff823e83b7b31a
UpcraftLP <j-schirra@gmx.de>
2019-03-21 03:10:17 +0100

    initial mod files

---
+++ src/main/java/nerdhub/steampunk/machinery/SteampunkMachinery.java
@@ -0,0 +1,13 @@
+package nerdhub.steampunk.machinery;
+
+import com.github.glasspane.mesh.api.annotation.CalledByReflection;
+import net.fabricmc.api.ModInitializer;
+
+@CalledByReflection
+public class SteampunkMachinery implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
